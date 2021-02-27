05e6687e144a1f1e3be0bbed4b3bf1bcdc2ad46a
Jack <jackstonemedia@gmail.com>
2021-02-27 15:32:56 +0000

    Add convertToResult

---
+++ Sources/CombineRx/Convenience Combine Operators/Publisher+ConvertToResult.swift
@@ -0,0 +1,20 @@
+//
+//  Publisher+ConvertToResult.swift
+//  Copyright © 2021 Jack Stone. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+extension Publisher {
+
+    /// Converts a publisher's output and failure type to a result that can be switched on.
+    /// The resulting publisher's error type becomes `Never` as any error will be caught and propagated to the `Result`.
+    ///
+    /// - Returns: A publisher whose original output and failure type become a result that can be sinked on, and whose failure type becomes `Never`.
+    public func convertToResult() -> AnyPublisher<Result<Output, Failure>, Never> {
+        return map(Result.success)
+            .catch { Just(.failure($0)) }
+            .eraseToAnyPublisher()
+    }
+}
---
+++ Tests/CombineRxTests/Convenience Combine Operators/Publisher+ConvertToResultTests.swift
@@ -0,0 +1,74 @@
+//
+//  Publisher+ConvertToResultTests.swift
+//  Copyright © 2021 Jack Stone. All rights reserved.
+//
+
+import XCTest
+import Combine
+@testable import CombineRx
+
+final class Publisher_ConvertToResultTests: XCTestCase {
+
+    private var subscriptions: Set<AnyCancellable>!
+
+    override func setUp() {
+        super.setUp()
+        subscriptions = Set<AnyCancellable>()
+    }
+
+    func testSuccessResultCase() {
+
+        let expectation = XCTestExpectation(description: "Should receive value when sinking")
+
+        let subject = PassthroughSubject<Int, TestError>()
+        let value = 99
+
+        var output = [Result<Int, TestError>]()
+
+        subject
+            .convertToResult()
+            .sink(receiveValue: { output.append($0); expectation.fulfill() })
+            .store(in: &subscriptions)
+
+        subject.send(value)
+
+        wait(for: [expectation], timeout: 0.1)
+
+        XCTAssertFalse(output.isEmpty)
+        XCTAssertEqual(output.count, 1)
+
+        switch output[0] {
+        case .failure(let error): XCTFail(error.localizedDescription)
+        case .success(let receivedValue): XCTAssertEqual(receivedValue, value)
+        }
+    }
+
+    func testFailureResultCase() {
+
+        let expectation = XCTestExpectation(description: "Should receive value when sinking")
+
+        let subject = PassthroughSubject<Int, TestError>()
+        let testError: TestError = .generic
+
+        var output = [Result<Int, TestError>]()
+
+        subject
+            .convertToResult()
+            .sink(receiveValue: { output.append($0); expectation.fulfill() })
+            .store(in: &subscriptions)
+
+        subject.send(completion: .failure(testError))
+
+        wait(for: [expectation], timeout: 0.1)
+
+        XCTAssertFalse(output.isEmpty)
+        XCTAssertEqual(output.count, 1)
+
+        switch output[0] {
+        case .failure(let error):
+            guard case .generic = error else { XCTFail(); return }
+        case .success:
+            XCTFail()
+        }
+    }
+}
