da714ef3e2bc543bb8ef8bf471c7de01195f72e0
Jack.Stone <Jack.Stone@global.com>
2022-05-28 13:30:43 +0100

    Update Publisher+AsObservable layout

--- Sources/CombineRx/Interoperability/Combine to RxSwift/Publisher+AsObservable.swift
+++ Sources/CombineRx/Interoperability/Combine to RxSwift/Publisher+AsObservable.swift
@@ -11,17 +11,19 @@ extension Publisher {
     /// A bridging function that transforms a Combine `Publisher` into an RxSwift `Observable`.
     ///
     /// - Returns: An RxSwift `Observable` that is the bridged transformation of the given `Publisher`.
+    ///
     public func asObservable() -> Observable<Output> {
         return .create { observer -> Disposable in
-            let cancellable = sink { completion in
-                switch completion {
-                case .finished:             observer.onCompleted()
-                case .failure(let error):   observer.onError(error)
-                }
 
-            } receiveValue: { value in
-                observer.onNext(value)
-            }
+            let cancellable = sink(
+                receiveCompletion: { completion in
+                    switch completion {
+                    case .finished:             observer.onCompleted()
+                    case .failure(let error):   observer.onError(error)
+                    }
+                },
+                receiveValue: { observer.onNext($0) }
+            )
 
             return Disposables.create {
                 cancellable.cancel()
