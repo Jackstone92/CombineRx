04318e62515c4e3a6c40cbd6c20a671431c2cdbb
Jack Stone <jackstonemedia@gmail.com>
2024-09-20 13:22:30 +0100

    Remove RxSwift 5 sample project and fix RxSwift 6 sample project (#13)
    * Remove RxSwift 5 sample project
    
    * Update Package.resolved
    
    * Add NumberFactClient
    
    * Remove Expression model and MathsExpressionClient
    
    * Update AppTransportSecurity to allow HTTP requests to numbersapi.com
    
    * Update ViewModel following switch to random number fact
    
    * Update ViewController following switch to random number fact
    
    * Remove unneeded spacing
    
    * Remove GitHub workflow for building legacy project

--- Sample Projects/RxSwift 6 App/RxSwift 6 App/ViewModel.swift
+++ Sample Projects/RxSwift 6 App/RxSwift 6 App/ViewModel.swift
@@ -10,55 +10,54 @@ import RxSwift
 import CombineRx
 
 final class ViewModel {
-
     enum State: Equatable {
         case pending
         case loading
-        case loaded(Expression)
+        case loaded(fact: String)
         case error
     }
 
-    var expressionAndAnswerObservable: Infallible<String> {
+    private(set) lazy var factInfallible: Infallible<String> = {
         stateSubject
             .skip { $0 == .pending }
-            .map { state in
-                guard let expression = state.expression else { return "" }
-                return [expression.expression, "=", String(expression.answer)].joined(separator: " ")
-            }
+            .compactMap(\.fact)
             .asInfallible(onErrorJustReturn: "")
-    }
+    }()
 
-    var isErrorObservable: Infallible<Bool> {
+    private(set) lazy var isErrorInfallible: Infallible<Bool> = {
         stateSubject
             .skip { $0 == .pending }
             .map { $0 == .error }
             .asInfallible(onErrorJustReturn: false)
-    }
+    }()
 
-    var isLoadingObservable: Infallible<Bool> {
+    private(set) lazy var isLoadingInfallible: Infallible<Bool> = {
         stateSubject
             .skip { $0 == .pending }
             .map { $0 == .loading }
             .asInfallible(onErrorJustReturn: false)
-    }
+    }()
 
-    var title: String { "Maths Expressions" }
-    var buttonLabel: String { "Random Expression" }
+    var title: String { "Random Number Facts" }
+    var buttonLabel: String { "Random Number Fact" }
     var errorMessage: String { "Something went wrong... Please try again" }
 
     let buttonTapSubject = PublishSubject<Void>()
 
     private let stateSubject = BehaviorSubject<State>(value: .pending)
 
-    private let client: MathsExpressionClient
+    private let client: NumberFactClient
+    private let randomNumberGenerator: () -> Int
     private let mainScheduler: SchedulerType
     private let disposeBag = DisposeBag()
 
     init(
-        client: MathsExpressionClient,
+        client: NumberFactClient,
+        randomNumberGenerator: @escaping () -> Int,
         mainScheduler: SchedulerType
     ) {
         self.client = client
+        self.randomNumberGenerator = randomNumberGenerator
         self.mainScheduler = mainScheduler
 
         subscribeToButtonTapObservable()
@@ -67,20 +66,20 @@ final class ViewModel {
     private func subscribeToButtonTapObservable() {
         buttonTapSubject
             .debounce(.milliseconds(300), scheduler: mainScheduler)
-            .subscribe(onNext: { [unowned self] in self.fetchRandomExpression() })
+            .subscribe(onNext: { [unowned self] in self.fetchRandomFact() })
             .disposed(by: disposeBag)
     }
 
-    private func fetchRandomExpression() {
+    private func fetchRandomFact() {
         guard let value = try? stateSubject.value(), value != .loading else { return }
 
         stateSubject.onNext(.loading)
 
-        client.randomExpression()
+        client.fact(randomNumberGenerator())
             .asObservable()
             .observe(on: mainScheduler)
             .subscribe(
-                onNext: { [unowned self] expression in self.stateSubject.onNext(.loaded(expression)) },
+                onNext: { [unowned self] fact in self.stateSubject.onNext(.loaded(fact: fact)) },
                 onError: { [unowned self] _ in self.stateSubject.onNext(.error) }
             )
             .disposed(by: disposeBag)
@@ -88,14 +87,8 @@ final class ViewModel {
 }
 
 private extension ViewModel.State {
-
-    var expression: Expression? {
-        switch self {
-        case .loaded(let expression):
-            return expression
-
-        case .pending, .loading, .error:
-            return nil
-        }
+    var fact: String? {
+        guard case let .loaded(fact) = self else { return nil }
+        return fact
     }
 }
